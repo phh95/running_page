53e1d6c7112983e9d828c4e2e100b2694c6171e0
phh95 <602646761@qq.com>
2022-05-22 21:32:31 +0800

    Update pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pathPrefix: `/`, // Change to `/running_page` when running on github pages
+    pathPrefix: `/running_page`, // Change to `/running_page` when running on github pages
     siteMetadata: {
         siteTitle: '彭宏豪的跑步记录',     
         siteUrl: 'https://running-page-jet.vercel.app/',         
